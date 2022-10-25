74483f6f38216486d622cfd53eb7c34f2b651f73
Karolina <kpomian@linux-p51-10.inf.ug.edu.pl>
2022-10-25 15:39:56 +0200

    class node

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
-#test
+class Node
+    attr_accessor :x, :y
+    
+    def initialize(x,y)
+        @x = x
+        @y = y
+    end
+end
+
+node = Node.new(1, 1)
+puts "#{node.x} #{node.y}"
+node.x = 3
+puts "#{node.x} #{node.y}"
+
+#hash life <- algorytm do kodowania struktur drzewiastych
